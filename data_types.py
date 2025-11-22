1814ec77d9dba7fbc2bd5ddb01c0e57105bdad0c
rishithpatel7-boop <rishithpatel7@gmail.com>
2025-11-22 09:52:57 +0530

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,16 @@
+#data types - in python 3 data types
+#1.int(integer)
+#2.str(string)
+#3float(decimals)
+name="venkat123" 
+age=30
+salary=10000.52
+data="1234"
+print(name)
+print(type(name))
+print(age)
+print(type(age))
+print(salary)
+print(type(salary))
+print(data)
+print(type(data))
---
+++ variables.py
@@ -0,0 +1,8 @@
+#variables-is a allocation to value
+name="venkat"
+age=31
+salary=1000.00
+print(name)
+print(age)
+print(salary)
+
